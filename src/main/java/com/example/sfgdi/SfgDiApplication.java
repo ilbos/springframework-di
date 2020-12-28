313422948e8128a129972f2631a0536a4edd8e7f
Ilgar Bosatov <ilgar.bosatov@gmail.com>
2020-12-28 21:01:18 +0100

    added annotation to SetterInjectedController

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -2,6 +2,7 @@ package com.example.sfgdi;
 
 import com.example.sfgdi.controllers.MyController;
 import com.example.sfgdi.controllers.PropertyInjectedController;
+import com.example.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,17 +15,16 @@ public class SfgDiApplication {
 		ConfigurableApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
 		MyController myController = (MyController) ctx.getBean("myController");
-
 		String greeting = myController.sayHello();
-		
 		System.out.println(greeting);
 
 		System.out.println("--------- Property");
-
 		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
 		System.out.println(propertyInjectedController.getGreeting());
 
+		System.out.println("--------- Setter");
+		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+		System.out.println(setterInjectedController.getGreeting());
 	}
 
 }
--- src/main/java/com/example/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/com/example/sfgdi/controllers/SetterInjectedController.java
@@ -1,12 +1,15 @@
 package com.example.sfgdi.controllers;
 
 import com.example.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class SetterInjectedController {
 
-    //private property will be set by the setter
     private GreetingService greetingService;
 
+    @Autowired
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
